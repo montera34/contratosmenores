bb1876693fe3b98725c523bdaa2c3e52ad20e7e2
numeroteca <pablo@basurama.org>
2014-10-13 19:11:16 +0200

    Añade línea de tope por arriba para valores superiores a 10.000€

--- js/dataviz.js
+++ js/dataviz.js
@@ -8,9 +8,10 @@ Also some ideas come from https://github.com/erhardt/Attention-Plotter
 var barwidth = 1.98; //width of the bars
 
 //Prepare canvas size
-var margin = {top: 35, right: 20, bottom: 100, left: 65},
+var margin = {top: 35, right: 20, bottom: 20, left: 65},
     width = 1300 - margin.left - margin.right,
-    height = 650 - margin.top - margin.bottom;
+    height = 550 - margin.top - margin.bottom;
+var topvalue = 10000;
 
 var formatComma = d3.format(",");
 
@@ -54,6 +55,58 @@ var barstimescale = svg.append('g').attr('id','barstimescale');
 //replaces spaces and . in viplist function(d) { return d.SaldoCalculado; }
 var replacement = function(d) { return d.replace(/\s+/g, '').replace(/\.+/g, '').toLowerCase();};
 
+//set top line
+var topline = svg.append('g').attr('class','topline');
+
+//Legend
+d3.tsv("data/viplist.tsv", function(error, data) {//reads the viplist.tsv file
+	var legend = d3.select("#legend").attr("class", "legend");
+	//legend.append("h5").style("font-weight","bold").text("Selecciona "); //legend title
+	legend.selectAll('div')
+		.data(data)
+		.enter().append("div")
+		.attr("class", function(d) { return "inactive btn btn-default btn-sm";})
+		.text(function(d) { return d.people + " ("+ d.entidad+") "; })
+		.on('click',function(d) { //when click on name
+			var personflat = replacement(d.people); //removes spaces and . from person name;
+			if (d3.select(this).attr('class')==='inactive btn btn-default btn-sm'){
+				//first time
+				svg.selectAll('svg .bar').style("opacity",.03);
+				svg.selectAll('svg .bar.'+personflat).style("opacity",.8);
+				d3.select(this).transition().duration(0).attr("class","btn-success btn btn-default btn-sm"); //adds class success to button
+			//second time
+			} else if (d3.select(this).attr('class')==='btn-success btn btn-default btn-sm'){
+				d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-sm";}); //removes .success class
+				svg.selectAll('svg .bar').style("opacity",.1);
+			}
+		}).append('img')
+		.attr('src', function(d) { return d.img; });
+}); //end read viplist.tsv file
+
+//Legend de cosas
+d3.tsv("data/thinglist.tsv", function(error, data) {//reads the viplist.tsv file
+	var legend = d3.select("#legendcosas").attr("class", "legendcosas");
+	//legend.append("h5").style("font-weight","bold").text("Selecciona "); //legend title
+	legend.selectAll('div')
+		.data(data)
+		.enter().append("div")
+		.attr("class", function(d) { return "inactive btn btn-default btn-sm";})
+		.text(function(d) { return d.cosa; })
+		.on('click',function(d) { //when click on name
+			var cosa = d.cosa;
+			if (d3.select(this).attr('class')==='inactive btn btn-default btn-sm'){
+				//first time
+				svg.selectAll('svg .bar').style("opacity",.01);
+				svg.selectAll('svg .bar.'+ cosa).style("opacity",.7);
+				d3.select(this).transition().duration(0).attr("class","btn-success btn btn-default btn-sm"); //adds class success to button
+			//second time
+			} else if (d3.select(this).attr('class')==='btn-success btn btn-default btn-sm'){
+				d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-sm";}); //removes .success class
+				svg.selectAll('svg .bar').style("opacity",.1);
+			}
+		});
+}); //end read thinglist.tsv file
+
 //Enters data.tsv and starts the graph-----------------------------------------
 d3.tsv("data/data.tsv", type, function(error, data) {//reads the data.tsv file
 	data.forEach(function(d) {
@@ -61,8 +114,7 @@ d3.tsv("data/data.tsv", type, function(error, data) {//reads the data.tsv file
   });
 	//Sets scales
   xScale.domain(d3.extent(data, function(d) { return d.date; })); //sets xScale depending on dates values
-  //yScale.domain(d3.extent(data, function(d) { return d.entradas; })).nice(); //sets yScale depending on entradas values
-	yScale.domain([0,16000]).nice(); //sets yScale depending on entradas values
+	yScale.domain([0,topvalue]).nice(); //sets yScale depending on entradas values
 
 	//Sets X axis 
 	svg.append("g")
@@ -82,33 +134,6 @@ d3.tsv("data/data.tsv", type, function(error, data) {//reads the data.tsv file
 		.attr("font-size","10")
 		.text("Euros");
 
-//Legend
-d3.tsv("data/viplist.tsv", function(error, data) {//reads the viplist.tsv file
-	var legend = d3.select("#legend").attr("class", "legend");
-	//legend.append("h5").style("font-weight","bold").text("Selecciona "); //legend title
-	legend.selectAll('div')
-		.data(data)
-		.enter().append("div")
-		.attr("class", function(d) { return "inactive btn btn-default btn-sm";})
-		.text(function(d) { return d.people + " ("+ d.entidad+") "; })
-		.on('click',function(d) { //when click on name
-			var personflat = replacement(d.people), //removes spaces and . from person name
-			    tipodonante = d.tipo,
-			    confirmado = d.confirmado;
-			if (d3.select(this).attr('class')==='inactive btn btn-default btn-sm'){
-				//first time
-				svg.selectAll('svg .bar').style("opacity",.04);
-				svg.selectAll('svg .bar.'+personflat).style("opacity",.8);
-				d3.select(this).transition().duration(0).attr("class","btn-success btn btn-default btn-sm"); //adds class success to button
-			//second time
-			} else if (d3.select(this).attr('class')==='btn-success btn btn-default btn-sm'){
-				d3.select(this).attr("class",function(d) { return "inactive btn btn-default btn-sm";}); //removes .success class
-				svg.selectAll('svg .bar').style("opacity",.1);
-			}
-		})append('img')
-		.attr('src', function(d) { return d.img; });
-}); //end read viplist.tsv file
-		
 	//Sets the bars with time scale
 	barstimescale.selectAll(".bar")
       	.data(data)
@@ -117,13 +142,16 @@ d3.tsv("data/viplist.tsv", function(error, data) {//reads the viplist.tsv file
 	.attr("opacity",.1)
 	.attr("class", 
 		function(d) {
-			return replacement(d.quien) + " bar " + d.operacion.toLowerCase() + (d.importe < 0 ? " negativo" : " positivo"); 
+			return replacement(d.quien) + " bar " + d.operacion.toLowerCase() + " " + d.actividad.replace(/\,+/g, '').toLowerCase() + " " + (d.importe < 0 ? " negativo" : " positivo"); 
 			//sets the name of the person without spaces as class for the bar and adds class negativo/positivo depending on value
 		}) 
 	.attr("x", function(d) { return xScale(d.date); })
 	.attr("width", barwidth+1)
 	.attr("y", function(d) { return yScale(Math.max(0, d.importe)); })
-	.attr("height", function(d) { return Math.abs(yScale(d.importe < 0 ? 0 : d.importe) - yScale(0)); })
+	.attr("y", function(d) { return yScale(Math.max(0, d.importe > topvalue ? topvalue : d.importe)); })
+	//.attr("height", function(d) { return d.importe > topvalue ? yScale(topvalue) : yScale(Math.max(0, d.importe)); })
+	.attr("height", function(d) { return Math.abs(yScale( d.importe > topvalue ? topvalue : d.importe) - yScale(0)); })
+	//.attr("height", function(d) { return Math.abs(yScale(d.importe < 0 ? 0 : d.importe) - yScale(0)); })
 	//The tooltips time scale
 		.on("mouseover", function(d) {      
 		    div.transition()        
@@ -141,9 +169,28 @@ d3.tsv("data/viplist.tsv", function(error, data) {//reads the viplist.tsv file
 
 });
 
+topline.append('line')
+	.attr('y1', 4)
+	.attr('y2', 4)
+	.attr('x1', 0)
+	.attr('x2', 1400)
+	.attr("class", "topline")
+	.on("mouseover", function(d) {
+	  div.transition()
+	      .duration(200)
+	      .style("opacity", .9);
+	  div.html("Gastos por encima de 10.000€" )
+	      .style("left", (d3.event.pageX) + "px") 
+	      .style("top", (d3.event.pageY) - 60 + "px");  
+	  })
+	.on("mouseout", function(d) { 
+	  div.transition()
+	      .duration(500)
+	      .style("opacity", 0);
+	});
+
+
 
 function type(d) {
-  d.entradas = +d.entradas;
-  d.saldo = +d.saldo;
   return d;
 }
